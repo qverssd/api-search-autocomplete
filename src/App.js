8b148861ec558761d5500c4cdafcf2c543903222
qverssd <aleksandr082005@gmail.com>
2024-05-04 10:18:05 +0300

    work done

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import SearchAutocomplete from './components/api-search-autocomplete';
+
+function App() {
+  return ( 
+    <div className="App">
+      <ScrollToSection/>
+    </div>
+  )
+}
+export default App;
